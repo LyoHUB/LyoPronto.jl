3355415262c00a80a2aa00e0e7b216b0c78fd489
Isaac Wheeler <ickaser@gmail.com>
2024-08-23 15:31:28 -0400

    First commit as separate package

---
+++ docs/make.jl
@@ -0,0 +1,36 @@
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+using DrWatson
+# @quickactivate "LyoPronto"
+using LyoPronto
+using Documenter
+
+# Here you may include files from the source directory
+# include(srcdir("dummy_src_file.jl"))
+
+@info "Building Documentation"
+makedocs(;
+    sitename = "LyoPronto.jl",
+    # This argument is only so that the sequence of pages in the sidebar is configured
+    # By default all markdown files in `docs/src` are expanded and included.
+    # pages = [
+    #     "index.md",
+    # ],
+    # Don't worry about what `CI` does in this line.
+    format = Documenter.HTML(prettyurls = CI),
+)
+
+@info "Deploying Documentation"
+if CI
+    deploydocs(
+        # `repo` MUST be set correctly. Once your GitHub name is set
+        # the auto-generated documentation will be hosted at:
+        # https://PutYourGitHubNameHere.github.io/LyoPronto.jl/dev/
+        # (assuming you have enabled `gh-pages` deployment)
+        repo = "github.com/lyohub/LyoPronto.jl.git",
+        target = "build",
+        push_preview = true,
+        devbranch = "main",
+    )
+end
+
+@info "Finished with Documentation"
---
+++ docs/src/index.md
@@ -0,0 +1,6 @@
+# LyoPronto.jl
+This is the mandatory `index.md` file that is the homepage of the documentation.
+
+
+## Subsection name
+Test.
